04e0e8830a04d49d233cf54655e94a294d55d4f5
chen tao <sars17173@hotmail.com>
2023-05-18 00:26:29 +0800

    1

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,12 +1,30 @@
 from math import radians
+from tkinter import END
 
 import numpy as np # installed with matplotlib
 import matplotlib.pyplot as plt
 
+
+
+
+#生成一个冒泡排序函数
+def bubble_sort(num):
+    for i in range(len(num)-1):
+        for j in range(len(num)-1-i):
+            if num[j] > num[j+1]:
+                num[j], num[j+1] = num[j+1], num[j]
+    return num
+
+
+
+
+
 def main():
-    xss = np.arange(0, radians(1800), radians(12))
-    plt.plot(xss, np.cos(xss), 'b')
-    plt.show()
+    num1 = 11
+    num2 = 2345
+    #str1 = "我是要拼接%s和%s" % (num1, num2)
+    print("我是要拼接%s和%s" % (num1, num2))
+
 
 
 main()
