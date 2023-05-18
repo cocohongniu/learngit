3d9a30e2f53792e98e47a60a2e9d2fb3b810f715
chen tao <sars17173@hotmail.com>
2023-05-18 23:55:33 +0800

    2

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,31 +1,55 @@
 from math import radians
 from tkinter import END
 
-import numpy as np # installed with matplotlib
+import numpy as np  # installed with matplotlib
 import matplotlib.pyplot as plt
 
 
-
-
-#生成一个冒泡排序函数
+# 冒泡排序
 def bubble_sort(num):
-    for i in range(len(num)-1):
-        for j in range(len(num)-1-i):
-            if num[j] > num[j+1]:
-                num[j], num[j+1] = num[j+1], num[j]
+    for i in range(len(num)):
+        for j in range(len(num) - i - 1):
+            if num[j] > num[j + 1]:
+                num[j], num[j + 1] = num[j + 1], num[j]
     return num
 
 
+# 快速排序
+def quick_sort(num):
+    if len(num) < 2:
+        return num
+    else:
+        pivot = num[0]
+        less = [i for i in num[1:] if i <= pivot]
+        greater = [i for i in num[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
 
+
+# 比较整数的大小
+def compare(a, b):
+    if a > b:
+        return 1
+    elif a < b:
+        return -1
+    else:
+        return 0
+
+#生成指定的区间随机整数
+def random_int(a, b):
+    return np.random.randint(a, b)
 
 
 def main():
-    num1 = 11
-    num2 = 2345
-    #str1 = "我是要拼接%s和%s" % (num1, num2)
-    print("我是要拼接%s和%s" % (num1, num2))
-
-
+    num = 1
+    #num = [0, 5, 3]
+    # num = bubble_sort(num)
+    # print(num)
+    #if len(num) > 3 and 1 > 0:
+    #    print("hello")
+    #num1 = random_int(3, 30)
+    #print(num1)
 
 main()
 
+#print("11")
+#print("22")
